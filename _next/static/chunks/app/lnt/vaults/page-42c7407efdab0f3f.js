c40a6945dd95c7905677a36cecbf23f85188a24f
devzoofi <devzoofi@users.noreply.github.com>
2025-08-08 05:27:20 +0000

    deploy: zoofiio/zoofi-apps@4b48af0b451c1463c737155fcd2f954f00deda7c

---
+++ _next/static/chunks/app/lnt/vaults/page-42c7407efdab0f3f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[20],{33720:function(n,e,u){Promise.resolve().then(u.bind(u,53388))}},function(n){n.O(0,[2257,9891,4345,6358,8109,5097,6445,4850,7459,8468,1331,3606,3099,5049,1445,4972,518,2516,7694,7834,15,3813,7053,4195,6338,1744],function(){return n(n.s=33720)}),_N_E=n.O()}]);
